8470deff50334f4f179faa64bbd91a4295ee0fef
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-02-16 20:06:19 +0100

    Added SimpleJSON for managing encoding format Changed welcome message avoid using literal Initial structure of the program

--- Custom Text Encoder/Program.cs
+++ Custom Text Encoder/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using SimpleJSON;
+using System.Diagnostics;
 
 namespace Custom_Text_Encoder
 {
@@ -11,19 +13,13 @@ namespace Custom_Text_Encoder
     {
         static void Main()
         {
-
             string codecStatus = File.Exists("codec.json") ? "Loaded" : "Not Loaded";
-            Console.Write($@"Select Mode:
-1. Encode.
-2. Decode.
-3. Load Encoding Codec(Current status: {codecStatus}).
-4. Create Encoding Codec.
-5. Modify Encoding Codec.
-Choose an option: ");
+            Console.Write($"Select Mode:\n1. Encode.\n2. Decode.\n3. Load Encoding Codec(Current status: {codecStatus}).\n4. Create Encoding Codec.\n5. Modify Encoding Codec.\nChoose an option: ");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 0: //debug
-
+                    char[] normalChar = {}; string[] encodedChar = {};
+                    SetupArray(JSONNode.Parse(File.ReadAllText("codec.json")), normalChar, encodedChar);
                     Console.ReadLine();
                     break;
                 case 1:
@@ -36,13 +32,26 @@ Choose an option: ");
             }
         }
 
-        static string Chiper(string text, char[] normalChar, char[] encodedChar)
+        static string Chiper(string text, char[] normalChar, string[] encodedChar)
         {
             for (int i = 0; i < normalChar.Length; i++)
             {
-                text = text.Replace(normalChar[i], encodedChar[i]);
+                text = text.Replace(normalChar[i].ToString(), encodedChar[i]);
             }
             return text;
+        }
+
+        static void SetupArray(JSONNode jsonFile, char[] normalChar, string[] encodedChar)
+        {
+            List<char> keys = new List<char>();
+            List<string> values = new List<string>();
+            foreach (var item in jsonFile)
+            {
+                keys.Add(item.Key[0]);
+                values.Add(item.Value);
+            }
+            normalChar = keys.ToArray();
+            encodedChar = values.ToArray();
         }
     }
 }
